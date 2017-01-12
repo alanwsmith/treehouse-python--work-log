9fc150ac14e609a13f1d398bbc4225c8786db50a
Alan W. Smith <alan@alanwsmith.com>
2017-01-12 10:25:22 -0500

    Added is_number_in_range.

--- interface.py
+++ interface.py
@@ -6,6 +6,25 @@ class Interface:
     def state(self):
         return 'start'
 
+    def is_number_in_range(self, **kwargs):
+        """Checks to see if a given number is in a range of 
+        numbers.
+
+        This will also return false if something other than
+        a number is passed as the first parameter so it works
+        as error checking as well.
+        """
+        try:
+            number_as_int = int(kwargs['number'])
+        except ValueError:
+            return False
+        else:
+            if number_as_int in range(kwargs['lower_bound'], kwargs['upper_bound'] + 1):
+                return True
+            else:
+                return False
+
+
 
 if __name__ == '__main__':
     
--- test_interface.py
+++ test_interface.py
@@ -20,6 +20,18 @@ class InterfaceTest:
         interface = Interface()
         self.assert_equal('start', interface.state())
 
+    def test_is_number_in_range(self):
+        interface = Interface()
+        self.assert_equal(True, interface.is_number_in_range(number=1, lower_bound=1, upper_bound=2))
+        self.assert_equal(True, interface.is_number_in_range(number=2, lower_bound=1, upper_bound=2))
+        self.assert_equal(True, interface.is_number_in_range(number=5, lower_bound=4, upper_bound=5))
+        self.assert_equal(True, interface.is_number_in_range(number=5, lower_bound=4, upper_bound=7))
+        self.assert_equal(False, interface.is_number_in_range(number=5, lower_bound=1, upper_bound=4))
+        self.assert_equal(False, interface.is_number_in_range(number=0, lower_bound=1, upper_bound=4))
+        self.assert_equal(False, interface.is_number_in_range(number='', lower_bound=1, upper_bound=4))
+        self.assert_equal(False, interface.is_number_in_range(number='a', lower_bound=1, upper_bound=4))
+
+
 
 if __name__ == '__main__':
 
