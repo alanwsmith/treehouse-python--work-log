f4dea1138322ca62455d15f6ad5941d6a109aab4
Alan W. Smith <alan@alanwsmith.com>
2017-01-12 12:00:29 -0500

    Starting to build out the main TaskList which will hold individual Tasks.

--- task_list.py
+++ task_list.py
@@ -1,2 +1,13 @@
 class TaskList:
-    pass
+
+    def __init__(self):
+        
+        self.tasks = []
+
+
+if __name__ == '__main__':
+
+    from test_task_list import TaskListTest
+
+    task_list_tester = TaskListTest()
+    task_list_tester.run_tests()
---
+++ test_task_list.py
@@ -0,0 +1,26 @@
+import re
+
+from task_list import TaskList
+
+class TaskListTest:
+    def assert_equal(self, a, b):
+        if a != b:
+            raise ValueError("Expected: {} - Got: {}".format(a, b))
+
+    def run_tests(self):
+        for method in dir(self):
+            if re.match(r'test_', method):
+                getattr(TaskListTest, method)(self)
+        print("All tests passed")
+
+    def test_start_with_empty_list(self):
+        task_list = TaskList()
+        self.assert_equal(0, len(task_list.tasks))
+
+
+
+
+if __name__ == '__main__':
+
+    task_list_tester = TaskListTest()
+    task_list_tester.run_tests()
