5a157359b20dd4f54ec64ea8f219c35083617d21
Alan W. Smith <alan@alanwsmith.com>
2017-01-17 20:51:00 -0500

    Alphabetized methods.

--- test_task_list.py
+++ test_task_list.py
@@ -15,7 +15,6 @@ class TaskListTest:
             '{}/test-a.csv'.format(self.testing_dir) 
         ]
 
-
     def assert_equal(self, a, b):
         if a != b:
             raise ValueError("Expected: {} - Got: {}".format(a, b))
@@ -29,15 +28,26 @@ class TaskListTest:
                 getattr(TaskListTest, method)(self)
         print("All tests passed")
 
-    def test_start_with_empty_list(self):
-        task_list = TaskList()
-        self.assert_equal(0, len(task_list.tasks))
-
     def test_add_a_task(self):
         task_list = TaskList()
         task = Task()
         self.assert_equal(True, task_list.add_task(task))
         self.assert_equal(1, len(task_list.tasks))
+
+    def test_read_from_file(self):
+        task_list = TaskList()
+        self.assert_equal(True, task_list.read_from_file('tests/fixture-1.csv'))
+        self.assert_equal(7, len(task_list.tasks))
+        task = task_list.tasks[0]
+        self.assert_equal('2017-01-02', str(task.date))
+        self.assert_equal('Test Task One', task.name)
+        self.assert_equal(30, task.minutes)
+        self.assert_equal('This is a wonderful test task', task.notes)
+
+    def test_read_from_file_that_does_not_exist(self):
+        task_list = TaskList()
+        self.assert_equal(True, task_list.read_from_file('file-that-does-not-exist.csv'))
+        self.assert_equal(0, len(task_list.tasks))
 
     def test_save_list_to_file(self):
         """This is where the output is written. 
@@ -54,23 +64,9 @@ class TaskListTest:
         self.assert_equal(True, task_list.save_to_file(output_file_path))
         self.assert_equal(True, os.path.isfile(output_file_path))
         
-    def test_read_from_file(self):
+    def test_start_with_empty_list(self):
         task_list = TaskList()
-        self.assert_equal(True, task_list.read_from_file('tests/fixture-1.csv'))
-        self.assert_equal(7, len(task_list.tasks))
-        task = task_list.tasks[0]
-        self.assert_equal('2017-01-02', str(task.date))
-        self.assert_equal('Test Task One', task.name)
-        self.assert_equal(30, task.minutes)
-        self.assert_equal('This is a wonderful test task', task.notes)
-
-    def test_read_from_file_that_does_not_exist(self):
-        task_list = TaskList()
-        self.assert_equal(True, task_list.read_from_file('file-that-does-not-exist.csv'))
         self.assert_equal(0, len(task_list.tasks))
-
-         
-
 
 if __name__ == '__main__':
 
